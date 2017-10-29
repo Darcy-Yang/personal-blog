f8613becffbd050471bd0e19b614571bb93d497d
yangliangfei <yangliangfei@oneband.life>
2017-10-29 16:27:55 +0800

    [ADD]add article page

---
+++ src/views/Home/article.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+  <span>Hello</span>
+  <span>{{ this.$route.params.id }}</span>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'article'
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
